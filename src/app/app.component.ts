52a02fe5e31fbfeda9c610eba79a84e7e9621588
Eduardo Gallego <eduardo.gallego@bluu.es>
2018-11-11 03:14:13 +0100

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularTest';
+  title = 'Bienvenido a Angular';
+  curso:string = "Angular"
+  profesor:string = "Tío Raro"
 }
