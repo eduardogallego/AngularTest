b785c3dd23d8d663096c51f2bad59b64a720c2d7
Eduardo Gallego <eduardo.gallego@bluu.es>
2018-11-20 15:39:48 +0100

    no message

---
+++ src/typescript/app.js
@@ -0,0 +1,7 @@
+function saludar(nombre) {
+    console.log("Hola " + nombre.toLowerCase());
+}
+var wolverine = {
+    nombre: "Logan"
+};
+saludar(wolverine.nombre);
---
+++ src/typescript/app.ts
@@ -0,0 +1,9 @@
+function saludar(nombre: string) {
+  console.log("Hola " + nombre.toLowerCase());
+}
+
+var wolverine = {
+  nombre: "Logan"
+};
+
+saludar(wolverine.nombre);
